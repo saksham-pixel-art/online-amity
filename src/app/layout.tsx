6acfb8d6904a3b5bc56e034adb5e6fdb19b20ec0
saksham-pixel-art <saksham-pixel-art@github.com>
2026-06-03 12:13:18 +0530

    feat: Add Google Tag Manager (GTM-WVG66TDW) to all pages
    - Add GTM script in <head> section
    - Add GTM noscript iframe in <body> for non-JS users
    - Applies automatically to all pages via root layout
    - ID: GTM-WVG66TDW

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -398,6 +398,19 @@ export default function RootLayout({
         <meta name="google-site-verification" content="mYn5o6Lgmu55eq879llvZ_m8vQ57O-Rz8BLqyQMDU9Y" />
         {/* <meta name="msvalidate.01" content="YOUR_BING_VERIFICATION_CODE" /> */}
 
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WVG66TDW');
+            `,
+          }}
+        />
+
         {/* Google Analytics 4 */}
         <script
           async
@@ -450,6 +463,15 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased bg-background text-on-surface">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVG66TDW"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
